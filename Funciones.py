c6567db5be715f66736e8352bf86ac768078ce3d
Jose-Prince <joseprince148@gmail.com>
2023-11-05 08:44:52 -0600

    Feat: AWG to mm converter function

--- Funciones.py
+++ Funciones.py
@@ -4,3 +4,17 @@ import math
 def resistance(resistivity,length,diameter): #Recibe como parametros la resistividad del material, el largo del cable (m) y su diametro (mm)
     resistance = (4*resistivity*length)/(math.pi*(diameter*10**-3))
     return resistance
+
+#AWG Converter
+def converter(AWG): #Recibe el número de AWG en String
+    mm = 0
+    if AWG == "0000":
+        mm = 11.684
+    elif AWG == "000":
+        mm = 10.405
+    elif AWG == "00":
+        mm = 9.266
+    else :
+        mm = 0.127*92**((36-AWG)/39)
+
+    return round(mm, 3) 
