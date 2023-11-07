d4875e624b6137dbeaad42e3fe5ecc58d055de82
MrAndrewlol <40283828+MrAndrewlol@users.noreply.github.com>
2023-11-07 16:54:39 -0600

    doni

--- fisica.py
+++ fisica.py
@@ -2,68 +2,180 @@ import customtkinter as CTK
 import turtle
 from turtle import RawTurtle, ScrolledCanvas
 import tkinter as tk
-
+import Funciones as func
 import pruebas as pr
 
 CTK.set_appearance_mode("System")  # Modes: system (default), light, dark
-CTK.set_default_color_theme("green")  # Themes: blue (default), dark-blue, green
+CTK.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
+
+
+def calculardatos(largo, largoprefijo,diametro, voltaje, material):
+    if(int(largo) != 0 and diametro !=0 and int(voltaje) !=0):
+        print("exitos")
+        try:
+            integer_number = int(diametro)
+            largo = float(largo)*dicprefix[largoprefijo]
+            print(f"The integer value is: {integer_number}")
+            resistencia = func.resistance(float(dicmater[material]), float(largo), float(dicawg[diametro]))
+            resis.configure(text= " "+ str(resistencia) +" Ω")
+            corriente = func.current(float(voltaje), float(resistencia))
+            corr.configure(text=" " + str(corriente) + " A")
+            rapidez = func.dSpeed(float(corriente),1,float(diametro) )
+            rapi.configure(text= " " + str(rapidez) + " m/s" )
+            tiempo = func.time(float(largo),float(rapidez))
+            tiemp.configure(text=" "+ str(tiempo) + " s")
+            pr.cilindro(tortu2, largo, float(diametro))
+            pr.movelectron(tortu3, tortu4, tortu5, tortu6, float(diametro)/2, largo, tiempo%4, rapidez%11)
+        except ValueError:
+            print("The string does not represent a valid integer.")
+            resistencia = func.resistance(float(dicmater[material]), float(largo), float(dicawg[diametro]))
+            resis.configure(text= " "+ str(resistencia) +" Ω")
+            corriente = func.current(float(voltaje), float(resistencia))
+            corr.configure(text=" " + str(corriente) + " A")
+            rapidez = func.dspeed(float(corriente),1,float(diametro) )
+            rapi.configure(text= " " + str(rapidez) + " m/s" )
+            tiempo = func.time(float(largo),float(rapidez))
+            tiemp.configure(text=" "+ str(tiempo) + " s")
+            pr.cilindro(tortu2, largo, float(diametro))
+            pr.movelectron(tortu3, tortu4, tortu5, tortu6, float(diametro)/2, largo, tiempo%4, rapidez%11)
+
+        
+            
+
+        
+    
+
 
 
 ###Array
-prefix = ["M", "k", "h", "da", "d", "c", "m","μ", "n", "p" ]
-dicprefix = {  }
+prefix = ["M", "k", "h", "da", "d", "--","c", "m","μ", "n", "p" ]
+dicprefix = { "M": 1*10**6, "k": 1*10**3, "h": 1*10**2, "da": 1*10**1, "--": 1 , "d": 1*10**-1, "c": 1*10**-2, "m": 1*10**-3, "μ": 1*10*-6, "n": 1*10*-9, "p": 1*10*-12 }
 awg = ["AWG 4", "AWG 6","AWG 8","AWG 10", "AWG 12","AWG 14", "AWG 16", "AWG 18", "AWG 20", "AWG 22"]
-
+dicawg = {"AWG 4": 5.189, "AWG 6": 4.115,"AWG 8": 3.264,"AWG 10" : 2.588, "AWG 12" : 2.053,"AWG 14": 1.628, "AWG 16": 1.291, "AWG 18" : 1.024, "AWG 20" : 0.812, "AWG 22" : 0.643}
 mater = ["oro", "plata", "cobre", "aluminio", "grafito"]
+dicmater = {"oro":1, "plata" : 1, "cobre" :1, "aluminio": 1, "grafito": 1}
+dicmateresis = {"oro":2.35*10**-8, "plata" : 1.59*10**-8, "cobre" :1.79*10**-8, "aluminio": 2.82*10**-8, "grafito": 60.00*10**-8} #Resistividad ohms por metro
+
+
 
 
 
 app = CTK.CTk()  # create CTk window like you do with the Tk window
 app.geometry("1530x1080+0+0") #1920 -1080
+despx = 190
+
+app.columnconfigure(100, weight=0)
+app.rowconfigure(100, weight=0)
+
+##Marco para salidas
+frame = CTK.CTkFrame(app)
+frame.rowconfigure(25, weight=1)
+frame.columnconfigure(25, weight=1)
+frame.grid(row=4,column=25, columnspan= 25, rowspan=25, sticky= "n")
+
+##Marco para entradas
+entryframe = CTK.CTkFrame(app)
+entryframe.rowconfigure(25, weight=2)
+entryframe.columnconfigure(25, weight=2)
+entryframe.grid(row=4,column=0, columnspan= 25, rowspan=25, sticky= "n")
+
+
+
+
+#Marco para salidas
+
+# Use CTkButton instead of tkinter Button
+titulo = CTK.CTkLabel( app,text=" ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬", font=("Arial", 14))
+titulo.grid(row=0,column=0, columnspan = 50)
+titul1 = CTK.CTkLabel( app,text="● • ● • ● • ● ● • ● • ● • ● Rapidez de arrastre de los electrones • ● • ● • ● • ● ● • ● • ● • ●", font=("Arial", 16))
+titul1.grid(row=1,column=0, columnspan = 50)
+titulo2 = CTK.CTkLabel( app,text=" ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬", font=("Arial", 14))
+titulo2.grid(row=2,column=0, columnspan = 50)
+
 canvas = tk.Canvas(app)
-canvas.config(width=1200, height=700) ##Cambiar la resolucion de la pantalla de la tortuga
-canvas.grid(row=10,column=10,rowspan=40, columnspan=4 )
+canvas.config(width=1920, height=500) ##Cambiar la resolucion de la pantalla de la tortuga
+canvas.grid(row=3,column=0, columnspan=100)
 
 screen = turtle.TurtleScreen(canvas)
 tortugagrid = RawTurtle(screen) 
 
+tortu2 = RawTurtle(screen)
+tortu3 = RawTurtle(screen).color("yellow")
+tortu4 = RawTurtle(screen).color("yellow")
+tortu5 = RawTurtle(screen).color("yellow")
+tortu6 = RawTurtle(screen).color("yellow")
 
 
 
 
-# Use CTkButton instead of tkinter Button
-titulo = CTK.CTkLabel( app,text="Rapidez de arrastre de los electrones", font=("Arial", 30))
-titulo.grid(row=0, column = 4, columnspan=3)
 
+tituloentrys = CTK.CTkLabel( entryframe,text="Entradas", font=("Arial", 18))
+tituloentrys.grid(row=0,column=0, columnspan= 25)
 
+titulosalida = CTK.CTkLabel( frame,text="Salidas", font=("Arial", 18))
+titulosalida.grid(row=0,column=25, columnspan= 25)
+
+columndesp = 10
 ##Entradas de largo de alambre
-largoal = CTK.CTkLabel( app,text="Largo del alambre (metros)", font=("Arial", 16))
-largoal.grid(row=50, column = 4)
-entrylargo = CTK.CTkEntry(app)
-entrylargo.grid(row=50, column=5)
-optionmenu_var = CTK.StringVar(value="m")
-optionmenu = CTK.CTkOptionMenu(app,values=prefix, variable=optionmenu_var)
-optionmenu.grid(row=50, column=7)
+largoal = CTK.CTkLabel( entryframe,text="        Largo del alambre (metros)", font=("Arial", 14))
+largoal.grid(row=1, column = columndesp)
+entrylargo = CTK.CTkEntry(entryframe, placeholder_text="0.0 m")
+entrylargo.grid(row=2, column=columndesp)
+optionmenu_var = CTK.StringVar(value="n")
+optionmenu = CTK.CTkOptionMenu(entryframe,values=prefix, variable=optionmenu_var, width=10 )
+optionmenu.grid(row=2, column=columndesp+1, sticky="w")
 
 ##Entradas Diametro
-tawg = CTK.CTkLabel( app,text="Diámetro (mm)", font=("Arial", 16))
-tawg.grid(row=51, column = 4)
-awgentry = CTK.StringVar()
-awg = CTK.CTkComboBox(app, values=awg, state="normal", variable=awgentry )
-awg.grid(row=52, column = 4)
+tawg = CTK.CTkLabel( entryframe,text="Diámetro (mm)", font=("Arial", 14))
+tawg.grid(row=4, column = columndesp, sticky="s")
+awgentry = CTK.StringVar(value="AWG 4")
+awg = CTK.CTkComboBox(entryframe, values=awg, state="normal", variable=awgentry )
+awg.grid(row=5, column = columndesp)
 
 ##Entradas material Conductor
-cond = CTK.CTkLabel( app,text="Diámetro (mm)", font=("Arial", 16))
-cond.grid(row=53, column = 4)
-materia = CTK.StringVar()
-maters = CTK.CTkComboBox(app, values=mater, state="readonly", variable=materia ) ##escribi la densidads
-maters.grid(row=54, column = 4)
+cond = CTK.CTkLabel( entryframe,text="Material del conductor", font=("Arial", 14))
+cond.grid(row=4, column = columndesp+3, sticky="s")
+materia = CTK.StringVar(value="oro")
+maters = CTK.CTkComboBox(entryframe, values=mater, state="readonly", variable=materia ) ##escribi la densidads
+maters.grid(row=5, column = columndesp+3)
 
 ##Entradas voltaje aplicado
-voltage = CTK.CTkLabel( app,text="Voltaje a aplicar", font=("Arial", 16))
-voltage.grid(row=55, column = 4)
-voltd =  CTK.CTkEntry(app) ##escribi la densidads
-voltd.grid(row=56, column = 4)
+voltage = CTK.CTkLabel( entryframe,text="Voltaje a aplicar (V)", font=("Arial", 14))
+voltage.grid(row=1, column = columndesp+3)
+voltd =  CTK.CTkEntry(entryframe, placeholder_text="0.0 V") ##escribi la densidads
+voltd.grid(row=2, column = columndesp+3)
+
+
+CTK.CTkLabel( entryframe,text="\n             \n", font=("Arial", 14)).grid(row=3, column=columndesp+2) #Espaciado
+CTK.CTkLabel( entryframe,text="        ", font=("Arial", 14)).grid(row=7, column=columndesp+5) #Espaciado
+#BUTON
+buton = CTK.CTkButton(entryframe, text="Calcular y dibujar", command=lambda: calculardatos(entrylargo.get(), optionmenu_var.get(), awgentry.get(),voltd.get() ,maters.get()  ))
+buton.grid(row= 8, column=columndesp+1, sticky="n")
+
+##Parametros de Salida 
+
+##Resistencia del alambre
+CTK.CTkLabel( frame,text="Resistencia del alambre", font=("Arial", 16)).grid(row=1, column=25, columnspan= 25)
+resis = CTK.CTkLabel( frame,text="0 Ω", font=("Arial", 16))
+resis.grid(row=2,column=25, columnspan= 25)
+
+#Corriente
+CTK.CTkLabel( frame,text="Corriente", font=("Arial", 16)).grid(row=3, column=25, columnspan= 25)
+corr = CTK.CTkLabel( frame,text="0.0 A", font=("Arial", 16))
+corr.grid(row=4,column=25, columnspan= 25)
+
+
+#velocidad
+CTK.CTkLabel( frame,text="Rapidez de arrastre de los electrones", font=("Arial", 16)).grid(row=5, column=25, columnspan= 25)
+rapi = CTK.CTkLabel( frame,text=" 0 m/s", font=("Arial", 16))
+rapi.grid(row=6,column=25, columnspan= 25)
+
+#tiempo
+CTK.CTkLabel( frame,text="     Tiempo que le tomará a los electrones atravesar todo el alambre         ", font=("Arial", 16)).grid(row=7, column=25, columnspan= 25)
+tiemp = CTK.CTkLabel( frame,text=" 0 s", font=("Arial", 16))
+tiemp.grid(row=8,column=25, columnspan= 25)
+
+##APARTADO
 
 
 
--- pruebas.py
+++ pruebas.py
@@ -52,8 +52,8 @@ def ybar(tortuga,screensize):
     tortuga.goto(0,0)
     tortuga.pendown()
     tortuga.left(90)
-    tortuga.backward(screensize-40)
-    tortuga.forward((screensize-40)*2)
+    tortuga.backward(screensize/2)
+    tortuga.forward((screensize/2)*2)
     tortuga.right(90)
     tortuga.write("y+")
     tortuga.goto(0,0)
@@ -68,6 +68,30 @@ def zbar(tortuga,screensize):
     tortuga.right(30)
     tortuga.write("z+")
     tortuga.goto(0,0)
+
+
+
+def cilindro(tortuga, largo, diameter):
+    radius = diameter/2
+    tortuga.penup()
+    tortuga.goto(-largo/2,-radius)
+    tortuga.pendown()
+    tortuga.forward(largo*2)
+
+    tortuga.circle(radius)
+
+    tortuga.setheading(90)
+    tortuga.penup()
+    tortuga.forward(2*radius)
+    tortuga.pendown()
+    tortuga.setheading(180)
+
+    tortuga.forward(largo*2)
+
+    tortuga.circle(radius, extent=180)  #  Draw 1/2 of the bottom/circle
+
+    
+
 
 def pantalla(tortugagrid):
 # Set up the screen
@@ -86,4 +110,51 @@ def pantalla(tortugagrid):
     xbar(tortugagrid,screensize*1.9)
     ybar(tortugagrid,screensize*1.2)
     zbar(tortugagrid,screensize*1.5)
+
+def movelectron(tortuga, tortuga1, tortuga2,tortuga3, radio, largo, rapidez, tiempo, speed):
+    contador = 0
+    tortuga = tortuga1 = tortuga2 = tortuga3
+    while(contador  < tiempo):
+        if(rapidez >=0):
+            tortuga.penup()
+            tortuga.showturtle()
+            tortuga.speed(speed)
+            tortuga.goto(-largo/2, radio/2)
+            tortuga.forward(largo)
+            tortuga.hideturtle()
+            tortuga.back(largo)
+        if(rapidez <=0):
+            tortuga.showturtle()
+            tortuga.speed(speed)
+            tortuga.penup()
+            tortuga.goto(-largo/2, radio)
+            tortuga.forward(largo)
+            tortuga.forward(largo)
+            tortuga.back(largo)
+        contador = contador +1
+        
+
+# app = tk.Tk()  # create CTk window like you do with the Tk window
+# app.geometry("1530x1080+0+0") #1920 -1080
+# canvas = tk.Canvas(app) ##Cambiar la resolucion de la pantalla de la tortuga
+# canvas.pack()
+
+# screen = turtle.TurtleScreen(canvas)
+# canvas.config(width=2000, height=1080)
+# tortugagrid = RawTurtle(screen) 
+# tortucil = RawTurtle(screen) 
+# pantalla(tortugagrid)
+# cilindro(tortucil,500,100)
+# tortu1 = RawTurtle(screen, shape="circle")
+# tortu1.color("blue") 
+# tortu2 = RawTurtle(screen) 
+# tortu3 = RawTurtle(screen) 
+# tortu4 = RawTurtle(screen) 
+# movelectron(tortu1,tortu2, tortu3,tortu4 ,100/4, 500, 12, 2,1)
+
+
+
+    
+
+#app.mainloop()
 
